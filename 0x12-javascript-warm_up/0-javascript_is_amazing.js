9fe273e62efd69772ce53b39c66436a0d7ec803a
kenolykarichi <kenolykarichi369@gmail.com>
2023-06-13 02:25:52 -0700

    0x12. JavaScript - Warm up

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,1 @@
+const myVar = console.log("JavaScript is amazing”)
---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,9 @@
+able File  6 lines (5 sloc)  108 Bytes
+ 
+
+#!/usr/bin/node
+//  returns the addition of 2 integers.
+
+exports.add = function (a, b) {
+  return a + b;
+};
